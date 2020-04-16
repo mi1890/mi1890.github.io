3db709d00079d108f7366c9a073a2dfc850f38d1
Mr.z <mit1890@qq.com>
2020-04-17 00:26:23 +0800

    +[blog] init again

---
+++ precache-manifest.0953f1a1a7f6b8d84c2a6c342208f516.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300535a9cfae03c22b2c9c4115920589",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcb4d9ae9a5bbf7a76df",
+    "url": "/static/css/main.d9d72096.chunk.css"
+  },
+  {
+    "revision": "07ba274ba7bdd1eb6441",
+    "url": "/static/js/2.e590a142.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e590a142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb4d9ae9a5bbf7a76df",
+    "url": "/static/js/main.6b7f03f2.chunk.js"
+  },
+  {
+    "revision": "430711db2cc41196af7a",
+    "url": "/static/js/runtime-main.9798f641.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
